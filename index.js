670d19fc4f65d20baf48b7b5568aeda34aeab5c0
nirglus <nirgluskin@gmail.com>
2023-11-06 17:00:45 +0200

    add get and static methods

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs")
+})
 
 
 app.listen(port, () =>{
