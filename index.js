46ca0da2b8c47b63519ccdc09fa4ced7d63681e2
nirglus <nirgluskin@gmail.com>
2023-11-08 20:56:21 +0200

    render about& contact

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(express.static("public"));
 app.get("/", (req, res) =>{
     res.render("index.ejs")
 });
+app.get("/about", (req, res) =>{
+    res.render("about.ejs");
+});
+app.get("/contact", (req, res) =>{
+    res.render("contact.ejs");
+});
+
 
 
 app.listen(port, () =>{
